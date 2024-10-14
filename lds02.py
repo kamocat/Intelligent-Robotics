ec8dcaaf72f7867ed5b38e8b902acf9e6342f503
Marshal Horn <kamocat@gmail.com>
2024-10-13 20:44:40 -0700

    Added confidence to array

--- lds02.py
+++ lds02.py
@@ -9,8 +9,10 @@ port = next(port_list).device #Will throw StopIteration if there are no ports
 
 with serial.Serial(port, 115200, timeout=1) as ser:
     fig, ax = pyplot.subplots()
-    angles=np.ndarray(0)
-    distances=np.ndarray(0)
+    a=np.ndarray(0)
+    d=np.ndarray(0)
+    c=np.ndarray(0)
+    max_lim = 260
     while True:
         b = ser.read(47)
         def btoi(x): #Converts two bytes to integer
@@ -24,21 +26,31 @@ with serial.Serial(port, 115200, timeout=1) as ser:
         start_angle = btoi(3)
         end_angle = btoi(41) 
         if end_angle < start_angle:
-            angles *= np.pi/18000
-            x = np.sin(angles) * distances
-            y = np.cos(angles) * distances
+            a *= np.pi/18000
+            if True:
+                x = np.sin(a) * d
+                y = np.cos(a) * d
+            else:
+                x = np.sin(a) * c
+                y = np.cos(a) * c
             ax.clear()
-            ax.plot(x,y, '.')
+            ax.plot(x,y)
+            xlim = ax.get_xlim()
+            ylim = ax.get_ylim()
+            max_lim = max(map(abs, (max_lim,0) + xlim + ylim))
+            ax.set_xlim((-max_lim, max_lim))
+            ax.set_ylim((-max_lim, max_lim))
             fig.show(False)
             pyplot.pause(0.01)
             end_angle += 36000
-            angles=np.ndarray(0)
-            distances=np.ndarray(0)
-        angles = np.append(angles, np.linspace(start_angle, end_angle, num=12))
-        distances = np.append( distances, [btoi(x) for x in range(5,41,3)] )
+            a=np.ndarray(0)
+            d=np.ndarray(0)
+            c=np.ndarray(0)
+        a = np.append( a, np.linspace(start_angle, end_angle, num=12))
+        d = np.append( d, [btoi(x) for x in range(5,41,3)] )
+        c = np.append( c, [b[x] for x in range(7,41,3)])
         timestamp = btoi(43)
         dt = timestamp - time_prev
-        confidence = [b[x] for x in range(7,41,3)]
 
 
 
